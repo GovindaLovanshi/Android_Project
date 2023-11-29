997755820a82d2de12590d917e8c5616ab95990f
Govinda Lovanshi <govindalovanshi417@gmail.com>
2023-11-29 20:33:23 +0530

    My Android Project

--- Food_Application/app/src/main/java/com/example/food_application/MainActivity.kt
+++ Food_Application/app/src/main/java/com/example/food_application/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.food_application
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
@@ -7,10 +8,11 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var navController = findNavController(R.id.fargmentContainerView)
+        var navController = findNavController(R.id.fragmentContainerView)
         var bottomNav = findViewById<BottomNavigationView>(R.id.bottoNavigationView)
         bottomNav.setupWithNavController(navController)
     }
--- Food_Application/settings.gradle.kts
+++ Food_Application/settings.gradle.kts
@@ -10,8 +10,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-    }
+
+        }
 }
 
 rootProject.name = "Food_Application"
 include(":app")
+
